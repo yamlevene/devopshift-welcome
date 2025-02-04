13b1eaea1abcf316f3f121ddda99c0c7b34aa7ff
Your Name <you@example.com>
2025-02-04 13:45:14 +0000

    added time_sleep for outputing the public ip

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -33,7 +33,21 @@ resource "aws_instance" "vm" {
   }
 }
 
+terraform {
+  required_providers {
+    time = {
+      source  = "hashicorp/time"
+      version = "0.7.2"  # Make sure to use the version that match latest version
+    }
+  }
+}
+
+resource "time_sleep" "wait_for_ip" {
+  create_duration = "10s"  # Wait for 10 seconds
+}
+
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
+  depends_on  = [time_sleep.wait_for_ip]  # Wait for the time_sleep resource to complete
   description = "Public IP address of the VM"
 }
