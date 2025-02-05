8702aa801857e1b1ca61d6e6d742a3d567f4deba
Your Name <you@example.com>
2025-02-05 12:08:35 +0000

    added datalab of data resources

---
+++ labs/lab102/labdata/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+data "aws_instances" "vms" {
+  filter {
+    name = "tag:Name"
+    values = ["yaniv-vm"]
+  }
+}
+
+data "aws_instance" "yaniv_vm" {
+  instance_id = "i-09df7e0ed385f871b"
+}
+
+# output "public_ips" {
+#   value = data.aws_instances.vms.public_ips
+# }
+
+output "public_ip" {
+  value = data.aws_instance.yaniv_vm.public_ips
+}
--- labs/lab102/part1/main.tf
+++ labs/lab102/part1/main.tf
@@ -1,3 +1,11 @@
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
 variable "dummy" {
   default = ""
 }
