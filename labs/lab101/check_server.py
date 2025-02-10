c6db636236dbb42107788552b5cc8630cb98587e
Your Name <you@example.com>
2025-02-10 09:13:51 +0000

    adjusted user input validation in lab101

--- labs/lab101/check_server.py
+++ labs/lab101/check_server.py
@@ -2,7 +2,13 @@ servers = {"server1": True, "server2": False}
 
 
 def check_server():
+
     server_input = input("please supply a server name:\n")
+
+    if server_input == "":
+        print("server name can't be empty")
+        return
+
     try:
         running = servers[server_input]
         print("running") if running else print("not running")
